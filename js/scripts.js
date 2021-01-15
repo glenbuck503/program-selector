cb3f174e4777371df1c7d7683136e7689c19d4c6
glenbuck503 <glenbuck503@gmail.com>
2021-01-15 10:41:48 -0800

    debate changine input forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
   $("form#selector").submit(function() {
     event.preventDefault();
-    const questonOne = $("input:radio[name=question1]:checked").val();
-    const questonTwo = $("input:radio[name=question2]:checked").val();
-    const questonThree = $("input:radio[name=question3]:checked").val();
-    const questonFour = $("input:radio[name=question4]:checked").val();
-    const questonFive = $("input:radio[name=question5]:checked").val();
+    const questionOne = $("input:radio[name=question1]:checked").val();
+    const questionTwo = $("input:radio[name=question2]:checked").val();
+    const questionThree = $("input:radio[name=question3]:checked").val();
+    const questionFour = $("input:radio[name=question4]:checked").val();
+    const questionFive = $("input:radio[name=question5]:checked").val();
+    const total = (questionOne + questionTwo + questionThree + questionFour + questionFive);
+    
+    console.log(total);
+
+    // if (total > 3) {
+    //   $("#rust").show();
+    // }else if ( total < 1) {
+    //   $("#go").show();
+
+    // }
+
+      
+
+
+    
 
 
 
-    $("#output").text();
+
+
+    $("#rust").hide();
+    $("#go").hide();
+    $("#python").hide();
+    $("#ruby").hide();
   });
 });
