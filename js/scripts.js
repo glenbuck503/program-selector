866fa1bdf5c98a6c7d8c2eb30cad69112283a905
glenbuck503 <glenbuck503@gmail.com>
2021-01-15 10:48:46 -0800

    first if statement working

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,22 @@
 $(document).ready(function() {
   $("form#selector").submit(function() {
     event.preventDefault();
-    const questionOne = $("input:radio[name=question1]:checked").val();
-    const questionTwo = $("input:radio[name=question2]:checked").val();
-    const questionThree = $("input:radio[name=question3]:checked").val();
-    const questionFour = $("input:radio[name=question4]:checked").val();
-    const questionFive = $("input:radio[name=question5]:checked").val();
+    const questionOne = parseInt($("input:radio[name=question1]:checked").val());
+    const questionTwo = parseInt($("input:radio[name=question2]:checked").val());
+    const questionThree = parseInt($("input:radio[name=question3]:checked").val());
+    const questionFour = parseInt($("input:radio[name=question4]:checked").val());
+    const questionFive = parseInt($("input:radio[name=question5]:checked").val());
     const total = (questionOne + questionTwo + questionThree + questionFour + questionFive);
     
     console.log(total);
 
-    // if (total > 3) {
-    //   $("#rust").show();
-    // }else if ( total < 1) {
-    //   $("#go").show();
+    if (total >= 8) {
+      $("#rust").show();
+    } else if ( total < 7) {
+      $("#go").show();
 
-    // }
-
-      
-
+    }
 
     
-
-
-
-
-
-    $("#rust").hide();
-    $("#go").hide();
-    $("#python").hide();
-    $("#ruby").hide();
   });
 });
