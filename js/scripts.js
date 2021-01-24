efd90f9867be6c58aaa0ecae8fa5f4e54eb6ce11
glenbuck503 <glenbuck503@gmail.com>
2021-01-23 19:06:31 -0800

    fix multi line value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#selector").submit(function() {
     event.preventDefault();
+    
     const questionOne = parseInt($("input:radio[name=question1]:checked").val());
     const questionTwo = parseInt($("input:radio[name=question2]:checked").val());
     const questionThree = parseInt($("input:radio[name=question3]:checked").val());
@@ -14,43 +15,40 @@ $(document).ready(function() {
     $("#python").hide()
     $("#ruby").hide()
     $(".result").hide()
-    
-    
+  
     if (total === 5) {
       $("#ruby").prepend("Hi" + " " + userName + "!");
       $(".result").fadeIn();
       $("#ruby").fadeIn();
       ClearFields();
-
-      } else if ( total > 5 && total < 8) {
-      $("#go").prepend("Hi" + " " + userName + "!");
-      $(".result").fadeIn();
-      $("#go").fadeIn();
-      ClearFields();
-      
-      } else if (total >= 8 && total < 12){
-      $("#python").prepend("Hi" + " " + userName + "!");
-      $(".result").fadeIn();
-      $("#python").fadeIn();
-      ClearFields();
-    
-      } else if (total === 12){
-      $("#rust").text("Hi" + " " + userName + "!");
-      $(".result").fadeIn();
-      $("#rust").fadeIn();
-      ClearFields();
+    } else if ( total > 5 && total < 8) {
+    $("#go").prepend("Hi" + " " + userName + "!");
+    $(".result").fadeIn();
+    $("#go").fadeIn();
+    ClearFields();
+    } else if (total >= 8 && total < 12){
+    $("#python").prepend("Hi" + " " + userName + "!");
+    $(".result").fadeIn();
+    $("#python").fadeIn();
+    ClearFields();
+    } else if (total === 12){
+    $("#rust").prepend("Hi" + " " + userName + "!");
+    $(".result").fadeIn();
+    $("#rust").fadeIn();
+    ClearFields();
     }
   });
 
   $("form#code").submit(function() {
     event.preventDefault();
     let codeCheck = $.trim($("#codeCheck").val());
+    
   
     $(".codeResults").hide();
     $("#correct").hide();
     $("#no").hide();
 
-    if (codeCheck != "" && codeCheck == "$(\"body\").addClass(\"blue-background\");"){
+    if (codeCheck != "" && codeCheck == "$(\"body\").addClass(\"blue-background\");\n\code"){
       $("body").removeClass();
       $("body").addClass("blue-background");
     } else if (codeCheck != "" && codeCheck == "img class=\"pic\" src=\"img/ocean.jpeg\""){
